1a1a4c192f830a31b0394894a17c5bc1237bae13
Mauricio Selfene <m.selfene@gmail.com>
2022-04-26 03:21:11 -0400

    Entrega ejercicios tema 1

---
+++ src/com/company/tiposDatos.java
@@ -0,0 +1,24 @@
+package com.company;
+
+public class tiposDatos {
+
+    public static void main(String[] args) {
+
+        //numerico
+        byte valorTipoByte = 4;
+
+        //entoros
+
+        short varibleShort = 8;
+        int valorEntero = 10;
+        long variableLong = 100;
+
+        double variableDouble = 3.14;
+        float variableFloat = 5.5f;
+
+        boolean variable0 = false;
+        boolean variable1 = true;
+
+        String cadenaVariable = "Esto es una cadena que varia";
+    }
+}
